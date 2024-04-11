04ea1cf031f2b7c4d67dfb020992da5d323c9476
Joao Holanda <joaocmdt505@gmail.com>
2024-04-11 16:18:56 -0300

    :sparkles: feat: Criando a página de edição de tarefas

--- todos/views.py
+++ todos/views.py
@@ -1,10 +1,15 @@
-from django.views.generic import ListView, CreateView
+from django.views.generic import ListView, CreateView, UpdateView
 from django.urls import reverse_lazy
 from .models import Todo
 class TodoListView(ListView):
     model = Todo
 
 class TodoCreateView(CreateView):
+    model = Todo
+    fields = ["title", "deadline"]
+    success_url = reverse_lazy("todo_list")
+
+class TodoUpdateView(UpdateView):
     model = Todo
     fields = ["title", "deadline"]
     success_url = reverse_lazy("todo_list")
