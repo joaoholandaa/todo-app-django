b2d6663133658a0ecfc6054675b19edc4a1de46b
Joao Holanda <joaocmdt505@gmail.com>
2024-04-11 15:16:41 -0300

    :sparkles: feat: Criando a página de cadastro de tarefas

--- todos/views.py
+++ todos/views.py
@@ -1,6 +1,10 @@
-from django.shortcuts import render
+from django.views.generic import ListView, CreateView
+from django.urls import reverse_lazy
 from .models import Todo
+class TodoListView(ListView):
+    model = Todo
 
-def todo_list(request):
-    todos = Todo.objects.all()
-    return render(request, "todos/todo_list.html", {"todos": todos})
+class TodoCreateView(CreateView):
+    model = Todo
+    fields = ["title", "deadline"]
+    success_url = reverse_lazy("todo_list")
